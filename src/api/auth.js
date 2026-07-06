71bbb493c2a94ad243490333d45d7a2cc915c6d5
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-07-06 16:55:28 +0530

    Final commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // Pointing to your server endpoint
-const API_URL = 'http://localhost:3001/api/auth';
+// const API_URL = 'http://localhost:3001/api/auth';
+const API_URL=import.meta.env.VITE_API_URL || 'http://localhost:3001/api/auth';
+
 
 export const logUser = async (loginData) => {
     return await axios.post(`${API_URL}/login`, loginData);
